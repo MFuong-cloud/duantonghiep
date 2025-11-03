bb2cfcf2696140d53b87ca34b4c68275f4f9b5b0
Le Xuan Thuy <thuytg17112005@gmail.com>
2025-11-03 20:09:30 +0700

    call api Đăng nhập, Đăng kí

--- shop-app/src/app/(auth)/login/login-form.tsx
+++ shop-app/src/app/(auth)/login/login-form.tsx
@@ -1,106 +1,116 @@
 "use client";
 
-import {z} from "zod";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {useForm} from "react-hook-form";
-
-import {Button} from "@/components/ui/button";
-import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import {LoginBody, LoginBodyType} from "@/schemaValidations/auth.schema";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { LoginBody, LoginBodyType } from "@/schemaValidations/auth.schema";
 import Link from "next/link";
-import {Tooltip, TooltipContent, TooltipTrigger} from "@/components/ui/tooltip";
-
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-});
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { AuthService } from "./_services/services";
 
 export default function LoginForm() {
-    const form = useForm<LoginBodyType>({
-        resolver: zodResolver(LoginBody),
-        defaultValues: {
-            emailOrPhoneNumber: "",
-            password: "",
-        },
-    });
+  const router = useRouter();
+  const form = useForm<LoginBodyType>({
+    resolver: zodResolver(LoginBody),
+    defaultValues: {
+      emailOrPhoneNumber: "",
+      password: "",
+    },
+  });
 
-    function onSubmit(values: LoginBodyType) {
-        console.log(values);
+  const onSubmit = async (values: LoginBodyType) => {
+    console.log("Form gửi đi:", values);
+    const result = await AuthService.login(values.emailOrPhoneNumber, values.password);
+
+    if (result.ok) {
+      localStorage.setItem("authToken", result.payload.token);
+      toast.success("Đăng nhập thành công!");
+      router.push("/");
+    } else {
+      toast.error(result.payload.message || "Sai thông tin đăng nhập!");
     }
+  };
 
-    return (
-        <div>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-                    <FormField
-                        control={form.control}
-                        name="emailOrPhoneNumber"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Email hoặc số điện thoại</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        type="email"
-                                        placeholder="email@example.com"
-                                        className="h-11"
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="emailOrPhoneNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email hoặc số điện thoại</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="email@example.com"
+                    className="h-11"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Mật khẩu</FormLabel>
-                                <Tooltip>
-                                    <TooltipTrigger asChild>
-                                        <FormControl>
-                                            <Input
-                                                type="password"
-                                                placeholder="••••••••"
-                                                className="h-11"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                    </TooltipTrigger>
-                                    <TooltipContent>
-                                        <p>
-                                            Mật khẩu phải có ít nhất 8 ký tự, bao gồm số, chữ và 1 ký
-                                            tự đặc biệt
-                                        </p>
-                                    </TooltipContent>
-                                </Tooltip>
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Mật khẩu</FormLabel>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="••••••••"
+                        className="h-11"
+                        {...field}
+                      />
+                    </FormControl>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Mật khẩu phải có ít nhất 8 ký tự...</p>
+                  </TooltipContent>
+                </Tooltip>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
-
-                    <Button type="submit"
-                            className="w-full h-11 mt-6 font-semibold text-white
+          <Button
+            type="submit"
+            className="w-full h-11 mt-6 font-semibold text-white
                              bg-gradient-to-br from-amber-400 to-orange-500
                              rounded-lg shadow-md transition-all duration-300
-                             hover:scale-101 hover:shadow-lg hover:from-amber-500 hover:to-orange-600">
-                        Đăng nhập
-                    </Button>
-                </form>
-            </Form>
+                             hover:scale-101 hover:shadow-lg hover:from-amber-500 hover:to-orange-600"
+          >
+            Đăng nhập
+          </Button>
+        </form>
+      </Form>
 
-            <p className="text-center text-sm text-muted-foreground mt-6">
-                Chưa có tài khoản?
-                <Link
-                    href="/register"
-                    className="font-medium text-primary hover:underline"
-                >
-                    <span> Đăng ký </span>
-                </Link>
-                để nhận được nhiều ưu đãi{" "}
-            </p>
-        </div>
-    );
+      <p className="text-center text-sm text-muted-foreground mt-6">
+        Chưa có tài khoản?
+        <Link href="/register" className="font-medium text-primary hover:underline">
+          <span> Đăng ký </span>
+        </Link>
+        để nhận được nhiều ưu đãi{" "}
+      </p>
+    </div>
+  );
 }
--- shop-app/src/app/(auth)/register/register-form.tsx
+++ shop-app/src/app/(auth)/register/register-form.tsx
@@ -1,164 +1,190 @@
 "use client";
 
-import {z} from "zod";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {useForm} from "react-hook-form";
-
-import {Button} from "@/components/ui/button";
-import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import {RegisterBody, RegisterBodyType,} from "@/schemaValidations/auth.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  RegisterBody,
+  RegisterBodyType,
+} from "@/schemaValidations/auth.schema";
 import Link from "next/link";
-import {Tooltip, TooltipContent, TooltipTrigger,} from "@/components/ui/tooltip";
-
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-});
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { AuthService } from "./_services/services";
 
 export default function RegisterForm() {
-    const form = useForm<RegisterBodyType>({
-        resolver: zodResolver(RegisterBody),
-        defaultValues: {
-            name: "",
-            email: "",
-            phoneNumber: "",
-            password: "",
-            confirmPassword: "",
-        },
-    });
+  const router = useRouter(); // ✅ hook được gọi trong component
+  const form = useForm<RegisterBodyType>({
+    resolver: zodResolver(RegisterBody),
+    defaultValues: {
+      name: "",
+      email: "",
+      phoneNumber: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
 
-    function onSubmit(values: RegisterBodyType) {
-        console.log(values);
+  async function onSubmit(values: RegisterBodyType) {
+    try {
+      const result = await AuthService.register(
+        values.name,
+        values.email,
+        values.phoneNumber,
+        values.password,
+        values.confirmPassword
+      );
+
+      if (result.ok) {
+        toast.success("Đăng ký thành công!");
+        router.push("/login");
+      } else {
+        toast.error(result.payload.message || "Đăng ký thất bại!");
+      }
+    } catch (error) {
+      toast.error("Lỗi hệ thống. Vui lòng thử lại!");
     }
+  }
 
-    return (
-        <div>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-                    <FormField
-                        control={form.control}
-                        name="name"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Họ và tên</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        placeholder="Nguyễn Văn A"
-                                        className="h-11"
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Họ và tên</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Nguyễn Văn A"
+                    className="h-11"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        type="email"
-                                        placeholder="email@example.com"
-                                        className="h-11"
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="email@example.com"
+                    className="h-11"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-                    <FormField
-                        control={form.control}
-                        name="phoneNumber"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Số điện thoại</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        type="number"
-                                        placeholder="09*******"
-                                        className="h-11"
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+          <FormField
+            control={form.control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Số điện thoại</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    placeholder="09*******"
+                    className="h-11"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Mật khẩu</FormLabel>
-                                <Tooltip>
-                                    <TooltipTrigger asChild>
-                                        <FormControl>
-                                            <Input
-                                                type="password"
-                                                placeholder="••••••••"
-                                                className="h-11"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                    </TooltipTrigger>
-                                    <TooltipContent>
-                                        <p>
-                                            Mật khẩu phải có ít nhất 8 ký tự, bao gồm số, chữ và 1 ký
-                                            tự đặc biệt
-                                        </p>
-                                    </TooltipContent>
-                                </Tooltip>
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Mật khẩu</FormLabel>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="••••••••"
+                        className="h-11"
+                        {...field}
+                      />
+                    </FormControl>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>
+                      Mật khẩu phải có ít nhất 8 ký tự, bao gồm số, chữ và 1 ký
+                      tự đặc biệt
+                    </p>
+                  </TooltipContent>
+                </Tooltip>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Xác nhận mật khẩu</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="••••••••"
+                    className="h-11"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-                    <FormField
-                        control={form.control}
-                        name="confirmPassword"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Xác nhận mật khẩu</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        type="password"
-                                        placeholder="••••••••"
-                                        className="h-11"
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+          <Button
+            type="submit"
+            className="w-full h-11 mt-6 font-semibold text-white bg-gradient-to-br
+              from-amber-400 to-orange-500 rounded-lg shadow-md transition-all
+              duration-300 hover:scale-101 hover:shadow-lg hover:from-amber-500 hover:to-orange-600"
+          >
+            Đăng ký
+          </Button>
+        </form>
+      </Form>
 
-                    <Button
-                        type="submit"
-                        className="w-full h-11 mt-6 font-semibold text-white bg-gradient-to-br
-                            from-amber-400 to-orange-500 rounded-lg shadow-md transition-all
-                            duration-300 hover:scale-101 hover:shadow-lg hover:from-amber-500 hover:to-orange-600">
-                        Đăng ký
-                    </Button>
-                </form>
-            </Form>
-
-            <p className="text-center text-sm text-muted-foreground mt-6">
-                Đã có tài khoản?{" "}
-                <Link
-                    href="/login"
-                    className="font-medium text-primary hover:underline"
-                >
-                    Đăng nhập
-                </Link>
-            </p>
-        </div>
-    );
+      <p className="text-center text-sm text-muted-foreground mt-6">
+        Đã có tài khoản?{" "}
+        <Link href="/login" className="font-medium text-primary hover:underline">
+          Đăng nhập
+        </Link>
+      </p>
+    </div>
+  );
 }
