bbf53926b6a2905e696f8e1e3b41df7a0f6ba76e
Trong Nguyen <Trongndph52232@gmail.com>
2026-01-13 18:41:19 +0700

    update api categorycategory

--- duantonghiep-FE/src/api/categories/category.service.ts
+++ duantonghiep-FE/src/api/categories/category.service.ts
@@ -18,7 +18,7 @@ export const CategoryService = {
         }
     },
 
-     async getCategory(id: number): Promise<Category> {
+    async getCategory(id: number): Promise<Category> {
         try {
             const res = await api.get(`/categories/${id}`);
             return res.data;
@@ -26,6 +26,7 @@ export const CategoryService = {
             throw axios.isAxiosError(error) ? error.response?.data ?? error : error;
         }
     },
+
     async createCategory(data: Partial<Category>): Promise<Category> {
         try {
             const res = await api.post("/categories", data);
